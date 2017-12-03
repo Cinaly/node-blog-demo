09c3399ff63831ee5924b227af1d2caf2673a8a2
Cinaly <834518573@qq.com>
2017-12-03 17:58:09 +0800

    1

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,23 @@
 /**
  * Created by web-01 on 2017/12/3.
  */
+const db=require('../lib/db');
+
 module.exports=function (app) {
     app.get('/blog/:username', (req, res) => {
-        console.log(`username:${req.session.username}`);
-        res.render('blog', {session: req.session});
+        let sql='select p.*,u.uid,uname,avatar ' +
+            'from blog.post p right outer join blog.user u ' +
+            'on p.uid=u.uid where u.uid=? order by addtime desc';
+        db.pool.query(sql,[req.session.userId],(err,results,fields)=>{
+            if(err) throw err;
+            res.render('blog',{
+                session:req.session,
+                posts:results
+            })
+            console.log(results);
+        });
+
+     //   console.log(`username:${req.session.username}`);
+     //   res.render('blog', {session: req.session});
     })
 }
--- routes/post.js
+++ routes/post.js
@@ -2,31 +2,47 @@
  * Created by web-01 on 2017/12/3.
  */
 
-const db=require('../lib/db');
+const db = require('../lib/db');
 
 module.exports = function (app) {
     app.get('/post', (req, res) => {
         res.render('post', {
-            session: req.session,
-            success: req.flash('success'),
-            error: req.flash('error')}
+                session: req.session,
+                success: req.flash('success'),
+                error: req.flash('error')
+            }
         )
     })
 
-    app.post('/post/create',(req,res)=>{
-        let title=req.body.title;
-        let content=req.body.content;
-        let uid=req.session.userId;
-        let sql='insert into blog.post(title,content,uid) value(?,?,?)';
-        db.pool.query(sql,[title,content,uid],(err,results,fields)=>{
-            if(err) throw err;
-            if(results.affectedRows===1){
-                res.redirect('/blog/'+req.session.username);
+    app.post('/post/create', (req, res) => {
+        let title = req.body.title;
+        let content = req.body.content;
+        let uid = req.session.userId;
+        let sql = 'insert into blog.post(title,content,uid) value(?,?,?)';
+        db.pool.query(sql, [title, content, uid], (err, results, fields) => {
+            if (err) throw err;
+            if (results.affectedRows === 1) {
+                res.redirect('/blog/' + req.session.username);
 
-            }else{
-                res.flash('error','发布失败!');
+            } else {
+                res.flash('error', '发布失败!');
                 res.redirect('back');
             }
         });
+    })
+
+    app.get('/post/:pid', (req, res) => {
+        let pid = req.params.pid;
+        console.log(pid);
+        let sql = 'select p.*,u.uid,uname,avatar ' +
+            'from blog.post p right outer join blog.user u ' +
+            'on p.uid=u.uid where pid=?';
+        db.pool.query(sql, [pid], (err,results,fields) => {
+            if(err) throw err;
+            res.render('entry',{
+                session:req.session,
+                post:results[0]
+            })
+        })
     })
 }
